f6e6229a401225f4711cb4a752209f3f7b448ccc
Cleibson Almeida <nosbielcs@bol.com.br>
2013-03-20 19:10:56 -0300

    Update secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Mantonieta::Application.config.secret_token = '3fed8b6a0d4e34275d701b1f56f3bb491bd9f5a2147125a3fcff3e2601328b8eeab0d121355b793996f372e6f9982fa7cb6a0ae364ed39a60504839f522dc095'
+Mantonieta::Application.secret_token = '3fed8b6a0d4e34275d701b1f56f3bb491bd9f5a2147125a3fcff3e2601328b8eeab0d121355b793996f372e6f9982fa7cb6a0ae364ed39a60504839f522dc095'
